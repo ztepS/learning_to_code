2db3b6e30c325c86dbe7e92df2c7ece95af37db7
zrepS <zteps1@gmail.com>
2013-05-29 17:06:58 +0400

    111

--- coding/field.py
+++ coding/field.py
@@ -12,19 +12,19 @@ import math
 
 class arrow:
     def __init__(self,X,Y):
-        self.rotation = 90
+        self.rotation = 0
         self.X = X
         self.Y = Y
-        self.strength = 0.1
+        self.strength = 50
         ##self.rad = 10
     
     def draw(self):
-        pygame.draw.aaline(window,(255,255,255),((self.X+5),(self.Y+5)),((self.X+5)+math.cos(self.rotation*3.14/180)*5,(self.Y+5)+math.sin(self.rotation*3.14/180)*5))
-        print 
-        #pygame.draw.circle(window,(255, 255, 255),(self.X+5,self.Y+5),5)
+        pygame.draw.aaline(window,(255,255,255),((self.X+5),(self.Y+5)),((self.X+5)+math.cos(self.rotation*3.14/180)*self.strength/10,(self.Y+5)+math.sin(self.rotation*3.14/180)*self.strength/10))
+        #print pygame.draw.circle(window,(255, 255, 255),(self.X+5,self.Y+5),5)
         
-    def rotate(self,angle):
-        self.rotation +=angle
+    def rotate(self,angle,strength):
+        if(self.rotation>angle):self.rotation +=strength
+        else:self.rotation -=strength
         while(self.rotation<0):self.rotation+=360
         while(self.rotation>359):self.rotation-=360
 
@@ -56,10 +56,10 @@ class field:
             for j in i:
                 #print j.X
                 #summ=0
-                if(j.X/10+1<=self.x_count-1):j.rotate(self.previous_list[j.X/10+1][j.Y/10]*self.arrow_list[j.X/10+1][j.Y/10].strength)
-                if(j.Y/10+1<=self.y_count-1):j.rotate(self.previous_list[j.X/10][j.Y/10+1]*self.arrow_list[j.X/10][j.Y/10+1].strength)
-                if(j.Y/10-1>=0):j.rotate(self.previous_list[j.X/10][j.Y/10-1]*self.arrow_list[j.X/10][j.Y/10-1].strength)
-                if(j.X/10-1>=0):j.rotate(self.previous_list[j.X/10-1][j.Y/10]*self.arrow_list[j.X/10-1][j.Y/10].strength)
+                if(j.X/10+1<=self.x_count-1):j.rotate(self.previous_list[j.X/10+1][j.Y/10], self.arrow_list[j.X/10+1][j.Y/10].strength)
+                if(j.Y/10+1<=self.y_count-1):j.rotate(self.previous_list[j.X/10][j.Y/10+1], self.arrow_list[j.X/10][j.Y/10+1].strength)
+                if(j.Y/10-1>=0):j.rotate(self.previous_list[j.X/10][j.Y/10-1], self.arrow_list[j.X/10][j.Y/10-1].strength)
+                if(j.X/10-1>=0):j.rotate(self.previous_list[j.X/10-1][j.Y/10], self.arrow_list[j.X/10-1][j.Y/10].strength)
                 
                 j.draw()
                 pygame.display.flip()
@@ -83,8 +83,8 @@ window = pygame.display.set_mode((xCount*10,yCount*10))
     
 test.arrow_list[0][0].rotation = 90
 test.arrow_list[4][9].rotation = 270
-for i in range(1):
-    test.process_fields()
+
+test.process_fields()
     #test.print_rotation()    
     
 running = True
@@ -92,4 +92,8 @@ while running == True:
         for event in pygame.event.get():   
             if event.type == pygame.QUIT: 
                 sys.exit(0) 
+            if event.type == pygame.KEYDOWN:
+                key = pygame.key.get_pressed()
+                if key[pygame.K_q]:test.process_fields()
+                   
         
