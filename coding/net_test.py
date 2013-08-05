2e009562673020a0f9fcbe71b0c9b0965130b5cd
zrepS <zteps1@gmail.com>
2013-08-05 14:55:46 +0400

    Made changes to net_test

--- coding/net_test.py
+++ coding/net_test.py
@@ -4,14 +4,69 @@ Created on 05.08.2013
 @author: user
 '''
 import urllib2
+import cStringIO
+from PIL import Image
 import time
 import json
 
-finalUrl='http://maps.googleapis.com/maps/api/directions/json?origin=55.592554,37.663670&destination=55.653036,37.652340&sensor=false&departure_time='+str(int(time.time()))+'&mode=transit'
-#print finalUrl
-response = urllib2.urlopen(finalUrl)
-#html = response.read()
+originX=37.663670
+originY=55.592554
+originCoords = str(originY)+','+str(originX)
 
-#print html
-parsed=json.load(response)
-print parsed["routes"][0]["legs"][0]["duration"]['text']
+def ParseTime(time):
+    time=time.split(" ")
+    
+    if(len(time)==4):
+        timeNum=float(time[0])+float(time[2])/60
+    if(len(time)==2):
+        timeNum=float(time[0])/60
+    return timeNum
+    
+def GetTime(startX,startY,endX,endY):
+    originCoords = str(startY)+','+str(startX)
+    destinationCoords = str(endY)+','+str(endX)
+    finalUrl='http://maps.googleapis.com/maps/api/directions/json?origin='+originCoords+'&destination='+destinationCoords+'&sensor=false&departure_time='+str(int(time.time()))+'&mode=transit'
+    response = urllib2.urlopen(finalUrl)
+    parsed=json.load(response)
+    
+    if(parsed['status']<>"ZERO_RESULTS"):
+        #print parsed["routes"][0]["legs"][0]["duration"]['text']," | ",
+        duration = ParseTime(parsed["routes"][0]["legs"][0]["duration"]['text'])
+        print parsed['status']
+        return duration
+    else: return -1
+#destinationX=37.652340
+#destinationY=55.653036
+#destinationCoords = str(destinationY)+','+str(destinationX)
+
+result = range(10)
+for i in range(10):result[i]=range(10)
+
+markerString=""#"&markers=color:blue%7Clabel:C%7C55.592554,37.663670"
+
+for i in range(-3,3):
+    for j in range(-3,3):
+        destinationX=originX+0.02*i
+        destinationY=originY+0.02*j
+        
+        #print destinationY, destinationX
+        result[i][j]=GetTime(originX,originY,destinationX,destinationY)
+        time.sleep(3)
+        #color:"+color+"%7C"
+        if(abs(result[i][j]-0.8)<0.05):
+            markerString+="&markers="+str(destinationY)+","+str(destinationX)
+            #print result[i][j]
+        
+    print i
+print markerString
+print result
+#print GetTime(37.663670,55.592554,37.663670,55.592554)
+
+#ParseTime("1 hour 25 mins")
+
+
+imageString="http://maps.googleapis.com/maps/api/staticmap?center=55.592554,37.663670&zoom=12&size=640x640&maptype=roadmap"+markerString+"&sensor=false"
+
+pic = cStringIO.StringIO(urllib2.urlopen(imageString).read())
+img = Image.open(pic)
+img.show()
---
+++ roughness_afm (2).py
@@ -0,0 +1,71 @@
+import glob
+import numpy as np
+import scipy as sc
+import re
+#import scipy.stsci.convolve as ss
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_ylabel('PSD')
+ax.set_xlabel('frequency')
+
+list = glob.glob('*.txt')
+leg=[]
+for filename in list:
+	f = open(filename, 'r')
+	a = f.readlines()
+
+	# pick up x-scale
+	p = re.sub(",",".",a[5])
+	p = p.split(' ')
+	xs = eval(p[3])*1e-4
+	ys = xs
+	
+	# pick up z-scale 
+	p = re.sub(",",".",a[7])
+	p = p.split(' ')
+	zs = eval(p[3])*1e-3
+	
+	# pick up data
+	b = a[16:len(a)]
+	d = np.zeros(( len(b), len(np.fromstring(a[17], dtype=int, sep=' ')) ))
+	for n,x in enumerate(b):
+		d[n,:]=np.fromstring(x, dtype=int, sep=' ')
+	d = d*zs
+	
+	psdx=np.zeros([d.shape[0],d.shape[1]/2])
+	for i in np.arange(1.,d.shape[0]):
+		pp = d[i,:]
+		psd1 = (abs(sc.fft(pp)*np.conjugate(sc.fft(pp))))
+		psdx[i,:] = psd1[0:d.shape[1]/2]
+	psdx = sum(psdx,0)*(xs)/(5*psdx.shape[1]**2)
+	vx = np.arange( (1/(xs*d.shape[1])), (1/xs), (2/(xs*d.shape[1])) ) 
+	
+	psdy=np.zeros([d.shape[1],d.shape[0]/2])
+	for i in np.arange(1.,d.shape[1]):
+		pp = d[:,i]
+		psd1 = (abs(sc.fft(pp)*np.conjugate(sc.fft(pp))))
+		psdy[i,:] = psd1[0:d.shape[0]/2]
+	psdy = sum(psdy,0)*(ys)/(5*psdy.shape[1]**2)
+	vy = np.arange( (1/(ys*d.shape[0])), (1/ys), (2/(ys*d.shape[0])) )
+	
+	ax.loglog(vx,psdx)
+	leg.append(filename+'_x')
+	#ax.loglog(vy,psdy)
+	#leg.append(filename+'_y')
+	#ax.legend(filename)
+
+	
+list = glob.glob('*.dat')
+
+for filename in list:	
+	xrs = np.loadtxt(filename)
+	xrsx=xrs[:,0]
+	xrsy=xrs[:,1]
+	ax.loglog(xrsx,xrsy,'o-')
+	leg.append(filename)
+
+ax.legend(leg,'upper right')
+
+plt.show()
